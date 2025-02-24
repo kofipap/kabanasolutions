1e641f710dcd4140847d7828bdba184c16fc4f05
kofipap <popesolo88@gmail.com>
2025-02-24 09:03:11 +0000

    NewPP

---
+++ Google Sheet.js
@@ -0,0 +1,9 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzEqZM2HCke94QOZ96-Rqps-ZWA8Exoh3RcNvqzcytCu_zH8q9XxeLySAN_KozbYsI/exec'
+const form = document.forms['contact__form']
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, {method: 'POST', body: new FormData(form)})
+    .then(response => alert("Thank you for your submission! A doctor will reach out to via a Call/Watsapp. "))
+    .then(() => { window.location.reload();})
+    .catch(error => console.error('Error', error.message))
+})

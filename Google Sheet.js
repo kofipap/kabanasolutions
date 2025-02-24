6a96fbf2b34db73d42d1651e399875aa7e11ca54
kofipap <popesolo88@gmail.com>
2025-02-24 09:19:39 +0000

    newPP

--- Google Sheet.js
+++ Google Sheet.js
@@ -3,7 +3,7 @@ const form = document.forms['contact__form']
 form.addEventListener('submit', e => {
     e.preventDefault()
     fetch(scriptURL, {method: 'POST', body: new FormData(form)})
-    .then(response => alert("Thank you for your submission! A doctor will reach out to via a Call/Watsapp. "))
+    .then(response => alert("Thank you for your submission! A doctor will reach out to via a Call/Whatsapp. "))
     .then(() => { window.location.reload();})
     .catch(error => console.error('Error', error.message))
 })
